ff6ca3cda44bc24cff9d90e3867c105632bc9e0b
meanstack <meanstack9@gmail.com>
2024-02-09 17:41:58 +0530

    using async await

--- controller/files.js
+++ controller/files.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const createFile = (req, res) => {
+const createFile = async (req, res) => {
   const data = req.body;
   if (Object.keys(data).length === 0) {
     return res.status(400).send('No data provided');
@@ -12,28 +12,15 @@ const createFile = (req, res) => {
   try {
     const directoryPath = path.join(__dirname, '..', 'files');
     if (!fs.existsSync(directoryPath)) {
-      fs.mkdir(directoryPath, (err) => {
-        if (err) {
-          return res.status(500).send({
-            message: 'Error while creating directory',
-            err: err.message,
-          });
-        }
-      });
+      await fs.promises.mkdir(directoryPath);
     }
     const filePath = path.join(
       directoryPath,
       `data${new Date().getTime()}.json`
     );
     const jsonData = JSON.stringify(data);
-    fs.writeFile(filePath, jsonData, (err) => {
-      if (err) {
-        return res
-          .status(500)
-          .send({ message: 'Error while writing file', error: err.message });
-      }
-      return res.status(200).send({ message: 'file created successfully' });
-    });
+    await fs.promises.writeFile(filePath, jsonData);
+    res.status(200).send({ message: 'file created successfully' });
   } catch (error) {
     res.status(500).send({
       message: 'Internal server error',
@@ -41,18 +28,11 @@ const createFile = (req, res) => {
     });
   }
 };
-const getAllFiles = (req, res) => {
+const getAllFiles = async (req, res) => {
   try {
     const directoryPath = path.join(__dirname, '..', 'files');
-    fs.readdir(directoryPath, (err, files) => {
-      if (err) {
-        return res.status(500).send({
-          message: 'Error while reading directory',
-          error: err.message,
-        });
-      }
-      res.status(200).send({ fileNames: files });
-    });
+    const files = await fs.promises.readdir(directoryPath);
+    res.status(200).send({ fileNames: files });
   } catch (error) {
     res.status(500).send({
       message: 'Internal server error',
@@ -60,28 +40,21 @@ const getAllFiles = (req, res) => {
     });
   }
 };
-const getFileData = (req, res) => {
+const getFileData = async (req, res) => {
   try {
     const { fileName } = req.params;
     const filePath = path.join(__dirname, '..', 'files', fileName);
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        return res.status(500).send({
-          message: 'Internal server error',
-          error: err.message,
-        });
-      }
-      let jsonData;
-      try {
-        jsonData = JSON.parse(data);
-      } catch (error) {
-        return res.status(500).send({
-          message: 'Error while parsing json data',
-          error: error.message,
-        });
-      }
-      res.status(200).json({ data: jsonData });
-    });
+    const fileData = await fs.promises.readFile(filePath, 'utf8');
+    let jsonData;
+    try {
+      jsonData = JSON.parse(fileData);
+    } catch (error) {
+      return res.status(500).send({
+        message: 'Error while parsing json data',
+        error: error.message,
+      });
+    }
+    res.status(200).json({ data: jsonData });
   } catch (error) {
     res.status(500).send({
       message: 'Internal server error',
@@ -89,7 +62,7 @@ const getFileData = (req, res) => {
     });
   }
 };
-const updateFileData = (req, res) => {
+const updateFileData = async (req, res) => {
   try {
     const { fileName } = req.params;
     const data = req.body;
@@ -98,30 +71,16 @@ const updateFileData = (req, res) => {
     }
     const filePath = path.join(__dirname, '..', 'files', fileName);
     if (fs.existsSync(filePath)) {
-      fs.readFile(filePath, 'utf8', (readErr, fileData) => {
-        if (readErr) {
-          return res.status(500).send({
-            message: 'Error while reading the file',
-            error: readErr.message,
-          });
-        }
-        let jsonData;
-        try {
-          jsonData = JSON.parse(fileData);
-        } catch (parseError) {
-          return res.status(500).send({ message: 'Error while parsing data' });
-        }
-        const updatedData = { ...jsonData, ...data };
-        const updatedJsonData = JSON.stringify(updatedData);
-        fs.writeFile(filePath, updatedJsonData, (writeErr) => {
-          if (writeErr) {
-            return res.status(500).send({
-              message: 'Error updating file',
-              error: writeErr.message,
-            });
-          }
-        });
-      });
+      const fileData = await fs.promises.readFile(filePath, 'utf8');
+      let jsonDataFromFile;
+      try {
+        jsonDataFromFile = JSON.parse(fileData);
+      } catch (parseError) {
+        return res.status(500).send({ message: 'Error while parsing data' });
+      }
+      const updatedData = { ...jsonDataFromFile, ...data };
+      const updatedJsonData = JSON.stringify(updatedData);
+      await fs.promises.writeFile(filePath, updatedJsonData);
       res.status(200).send('File updated successfully');
     } else {
       return res.status(404).send({ message: 'File not found' });
@@ -134,23 +93,12 @@ const updateFileData = (req, res) => {
     });
   }
 };
-const deleteFile = (req, res) => {
+const deleteFile = async (req, res) => {
   const { fileName } = req.params;
   const filePath = path.join(__dirname, '..', 'files', fileName);
   try {
-    fs.unlink(filePath, (err) => {
-      if (err) {
-        if (err.code === 'ENOENT') {
-          return res.status(404).send({ message: 'File not found' });
-        } else {
-          return res.status(500).send({
-            message: 'Error while deleting file',
-            error: err.message,
-          });
-        }
-      }
-      res.status(200).send('File deleted successfully');
-    });
+    await fs.promises.unlink(filePath);
+    res.status(200).send('File deleted successfully');
   } catch (error) {
     res.status(500).send({
       message: 'Internal server error',
